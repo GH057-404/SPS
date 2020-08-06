6747d387c7b0e0cfc97ac67349850b84053060f8
GH057-404 <69310665+GH057-404@users.noreply.github.com>
2020-08-06 19:16:53 +0530

    Add files via upload

---
+++ stone_paper_scissor.py
@@ -0,0 +1,52 @@
+
+from art import *
+import random
+import tabulate 
+pat = text2art('''stone
+paper
+scissor''',"bulbhead")
+print(pat)
+print('For exiting the game press \'ctrl+z\'')
+options = ['stone', 'paper', 'scissor']
+count_co = 0
+count_us = 0
+while True:
+ print('\n\t\t\tPOINTS\n\n\t\tUser:     {}\n\t\tComputer: {}').format(count_us,count_co)
+ x = raw_input("\nEnter your choice: stone(1), paper(2) or scissor(3)?\n")
+ y = random.choice(options) 
+ if x == '1':
+   x = 'stone'
+ elif x == '2':
+   x = 'paper'
+ elif x == '3':
+   x = 'scissor'
+
+ if x == y:
+   print('user: '+x+'\n'+'computer: '+y)
+   print("It's a tie")
+ elif x == 'stone' and y == 'paper':
+   print('user: '+x+'\n'+'computer: '+y)
+   print('\nComputer wins!!')
+   count_co = count_co+1
+ elif x == 'stone' and y == 'scissor':
+   print('user: '+x+'\n'+'computer: '+y)
+   print('\nUser wins!!')
+   count_us = count_us+1
+ elif x == 'paper' and y == 'stone':
+   print('user: '+x+'\n'+'computer: '+y)
+   print('\nUser wins!!')
+   count_us = count_us+1
+ elif x == 'paper' and y == 'scissor':
+   print('user: '+x+'\n'+'computer: '+y)
+   print('\nComputer wins!!')
+   count_co = count_co+1
+ elif x == 'scissor' and y == 'stone':
+   print('user: '+x+'\n'+'computer: '+y)
+   print('\nComputer wins!!')
+   count_co = count_co+1
+ elif x == 'scissor' and y == 'paper':
+   print('user: '+x+'\n'+'computer: '+y)
+   print('\nUser wins!!')
+   count_us = count_us+1
+ else:
+   print('Invalid choice')
